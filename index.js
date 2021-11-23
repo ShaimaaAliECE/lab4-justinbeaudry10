fa7fd261c585cda2b2a3f38d1f6734470e1ce7f5
justinbeaudry10 <jbeaudr5@uwo.ca>
2021-11-23 17:26:56 -0500

    Added submit button, refactored to use jQuery

--- index.js
+++ index.js
@@ -6,9 +6,42 @@ const app = express();
 // Serve static contents
 app.use(express.static("static"));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/questions", (req, res) => {
   // Converts json object to string and sends as response
   res.json(questions);
+});
+
+app.post("/submit", (req, res) => {
+  // Sets number of questions to the length of the json object
+  const numQuestions = questions.length;
+
+  let answers = []; // Array of user answers
+  let correct = []; // Array of correct answers (from json object)
+  let score = 0; // Keeps count of user's correct answers
+
+  // Loops over each question in json object
+  questions.forEach((q) => {
+    // Pushes the correct answer's index to the 'correct' array
+    correct.push(q["answerIndex"]);
+  });
+
+  // Loops over all answers in json object
+  // Outer loop loops over each question
+  for (let q = 0; q < numQuestions; q++) {
+    // Inner loop loops over each option for a given question
+    for (let a = 0; a < questions[q]["options"].length; a++) {
+      // questions[q]["options"][a]
+    }
+  }
+
+  let markup = `<p>Grade: ${answers}/${numQuestions}</p>`;
+  res.send(markup);
 });
 
 // Port 80
---
+++ static/quiz.js
@@ -0,0 +1,46 @@
+let xReq = new XMLHttpRequest();
+
+// When the readyState property of the xReq changes, call the displayQuestions function
+xReq.onreadystatechange = function () {
+  // If the xReq is finished and the status is OK, display the questions
+  if (this.readyState == 4 && this.status == 200) {
+    // Gets questions from server
+    let questions = JSON.parse(this.responseText);
+
+    // When document is ready
+    $(document).ready(function () {
+      // Variable to keep track of added markup
+      let markup = `<form action="/submit" method="post">`;
+
+      // For each question
+      for (const [i, q] of questions.entries()) {
+        // Add the question stem as bold text
+        markup += `<p><b>${i + 1}. ${q.stem}</b></p>`;
+
+        // For each option for the question
+        for (const [num, opt] of q.options.entries()) {
+          // Add a radio button and a label
+          markup += `
+              <input type="radio" value="${opt}" name="q${i + 1}" id="q${
+            i + 1
+          }a${num + 1}" required>
+              <label for="q${i + 1}a${num + 1}">${opt}</label><br>
+              `;
+        }
+      }
+
+      // Submit button
+      markup += `<button style="margin-top: 1rem">Submit</button>
+                </form>`;
+
+      // Set the quiz-container div's html to the markup we created
+      $("#quiz-container").html(markup);
+    });
+  }
+};
+
+// GET /questions asynchronously
+xReq.open("GET", "/questions", true);
+
+// Send the xReq
+xReq.send();
